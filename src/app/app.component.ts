5ad1acd874331538210aa099f938a43eef3d9681
Phien Ngo <phien.ngo@terralogic.com>
2017-09-19 17:29:12 +0700

    Update load default data for all chart qhen load page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranserData } from './services/transerData.service';
+import { DataService } from './services/dataServices';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   chartData: any;
+  constructor(private _transferData: TranserData, private _dataService: DataService) {
+    let defaultFilter = {};
+    defaultFilter['startDate'] = '';
+    defaultFilter['endDate'] = '';
+    defaultFilter['reposModel'] = '';
+    defaultFilter['branchesModel'] = '';
+    defaultFilter['usersModel'] = '';
+    this._dataService.getChartData(JSON.stringify(defaultFilter), 'graph1').subscribe(res => {
+      this._transferData.updateChartFileChangeCommitData(res[0]);
+      this._transferData.updateChartJiraStatusCommitData(res[1]);
+      this._transferData.updateLineChangeOfCommitData(res[2]);
+      this._transferData.updateJiraTypeOfCommitData(res[3]);
+      this._transferData.updateHeatMapOfCommitData(res[4]);
+      this._transferData.updateJiraDefectOfCommitData(res[5]);
+    },
+      error => alert("error: Can't get chart data for graph"),
+      () => {
+        console.log("Finish");
+      }
+    );
+  }
 }
